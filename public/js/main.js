96d1005acd470521813c5badb843f249ef917881
charles de Lalaing <71701904+CharlesdeLalaing@users.noreply.github.com>
2021-04-15 12:11:41 +0200

    exo2java

---
+++ public/js/main.js
@@ -0,0 +1,42 @@
+//part1
+
+let phrase_une = "J’aime les tartines et j’en mange";
+console.log(phrase_une);
+
+let phrase_deux = "4 par jours";
+console.log(phrase_deux);
+
+console.log(phrase_une + " " + phrase_deux);
+
+let int_un = 5;
+console.log(int_un);
+
+let int_deux = 35;
+console.log(int_deux);
+
+console.log(int_un + int_deux);
+
+console.log(phrase_une + " " + int_un);
+
+//part2
+
+let phrase_trois = "salut tout le monde";
+console.log(phrase_trois);
+
+let phrase_quattre = "aurevoir !";
+console.log(phrase_quattre);
+
+phrase_trois = phrase_quattre;
+console.log(phrase_trois);
+
+//part3
+
+let int_trois = 4, string_un = "6";
+
+console.log(int_trois > string_un)
+console.log(int_trois < string_un)
+console.log(int_trois == string_un)
+console.log(int_trois != string_un)
+console.log(int_trois === string_un) //returns true if the elements are the same type and the same values
+console.log(int_trois !== string_un) //same type but not equal or different type
+
